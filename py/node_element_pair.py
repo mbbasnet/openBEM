d93b5d58bc72a9438e79dfe031345a822bbe917b
mbbasnet <minbasnet@gmail.com>
2022-01-12 18:00:13 +0545

    .

--- py/node_element_pair.py
+++ py/node_element_pair.py
@@ -17,17 +17,19 @@ def radius_param(col_pt, int_pt):
     r = np.linalg.norm(dxr) # radius as length of the vector
     dxr = dxr/r # normalize cx/dr
     return r, dxr
-
-if(True):
+        
+        
+def bem_elm_eldyn():
     '''
-    gauss integration for node element pair
-    For normal case (no singularity)
+    Computes element BEM matrices for each element node pair
+    for normal case i.e. collocation node not in the element
     '''
-    # Reset the local G and H matrices for new element
+    # Reset/initialize the local G and H matrices for new element
     G = 0.0
     Hs = 0.0
     H = 0.0
-
+    
+    
     # For normal element node pair (node outside element)
     for xsi in gauss_pt:
 
@@ -39,7 +41,7 @@ if(True):
         # Calculate radius r and derivative dx/dr = [dx1/dr, dx2/dr] between collocation and integration points
         r, dxr = radius_param(ncoord, int_pt)
         drn = np.dot(dxr, Un) # dr/dn
-
+        
         # Calculate reciprocal displacement and tractions for normal case (no singularity)
         funda.calculateDisplacement(dxr)
         funda.calculateTraction_dyn(r, dxr, Un, drn)
@@ -56,6 +58,42 @@ if(True):
         Hs += np.dot(Tdf,elm_par)
         H += np.dot(Ts,elm_par)
         
+def bem_elm_eldyn_q3_start():
+    '''
+    Computes element BEM matrices for each element node pair
+    for collocation node start node of the quadratic element
+    '''
+    # Reset/initialize the local G and H matrices for new element
+    G = 0.0
+    Hs = 0.0
+    H = 0.0
+    
+    
+    # Calculation of non-singular part of G matrix and H matrix
+    for xsi in gauss_pt:
+
+        # Calculate element parameters
+        Ni = serendip_1D(xsi, inci) # Shape function
+        jac, Un = jac_normal_1D(xsi, inci, elcoord) # Jacobian and unit normal
+        int_pt = np.matmul(Ni, elcoord) # Cartesian coordinate at the integration point
+
+        # Calculate radius r and derivative dx/dr = [dx1/dr, dx2/dr] between collocation and integration points
+        r, dxr = radius_param(ncoord, int_pt)
+        drn = np.dot(dxr, Un) # dr/dn
         
+        # Calculate reciprocal displacement and tractions for normal case (no singularity)
+        funda.calculateDisplacementSN(r, dxr, NDZ)
+        funda.calculateTraction_dyn(r, dxr, Un, drn)
+        funda.calculateTraction_st(r, dxr, Un, drn)
         
+        # create local variables of reciprocal displacement and tractions
+        Uf = funda.displacement
+        Tdf = funda.Traction_dyn
+        Ts = funda.Traction_st
+        
+        # Calculate G and H matrices
+        elm_par += jac*gauss_wt*Ni
+        G += np.dot(Uf,elm_par)
+        Hs += np.dot(Tdf,elm_par)
+        H += np.dot(Ts,elm_par)
         
